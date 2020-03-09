549bc4adf9e3f02ef9ba6f7b947479c693509b6b
fengming.shi <fengming.sfm@alibaba-inc.com>
2020-03-09 18:18:13 +0800

    fix: fix andriod prerelease bug

--- lib/big_keeper/dependency/dep_gradle_operator.rb
+++ lib/big_keeper/dependency/dep_gradle_operator.rb
@@ -201,8 +201,7 @@ module BigKeeper
       BigkeeperParser.parse("#{path}/Bigkeeper")
 
       version = BigkeeperParser.version if version == 'Version in Bigkeeper file'
-      modules = BigkeeperParser.module_names if (modules.nil? || modules.empty?)
-      modules = release_check_changed_modules(path, user) if (modules.nil? || modules.empty?)
+      modules = release_check_changed_modules(path, user) if (modules.nil? || modules.empty?
       if modules.nil? || modules.empty?
         Logger.error('no module need to release')
       end
